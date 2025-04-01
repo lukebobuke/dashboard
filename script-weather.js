84f6e18a35d03e2aae226a33252c12a470f4516b
lukebobuke <177164694+lukebobuke@users.noreply.github.com>
2025-04-01 00:30:23 -0500

    Transferred weather page script to separate file.
    Transferred weather page script to separate file.

---
+++ script-weather.js
@@ -0,0 +1,322 @@
+const root = document.documentElement
+const pinIcon = document.getElementById("pinIcon")
+const searchForm = document.getElementById("searchForm")
+const searchInput = document.getElementById("searchInput")
+const setButton = document.getElementById("setButton")
+const weatherButton = document.querySelectorAll(".weatherButton")
+const forecastDay = document.querySelectorAll(".forecastDay")
+const body = document.querySelector("body")
+const cityDisplay = document.getElementById("cityDisplay")
+const temp = document.querySelectorAll(".temp")
+const popContainer = document.querySelectorAll(".popContainer")
+const colorButtonContainer = document.querySelector("#colorButtonContainer")
+const colorButtons = colorButtonContainer.querySelectorAll("button")
+const palletButton = document.querySelector("#palletButton")
+
+var dailySummary = []
+var responseForecast = ""
+var dataForecast = ""
+var forecastArray = []
+var dataNow = []
+
+forecastDay.forEach(day => {
+    day.querySelector(".temp").style.visibility = "hidden"
+    day.querySelector(".popContainer").style.visibility = "hidden"
+})
+
+
+var city = "Chicago"
+cityDisplay.innerText = city
+const apiKey = "b310682931bcc24d7e4a32363c54e44d"
+
+async function checkWeather() {
+    const apiURLNow = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${apiKey}`
+    const apiURLForecast = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${city}&appid=${apiKey}`
+    
+    const responseNow = await fetch(apiURLNow)
+    if (!responseNow.ok) {
+        console.log("Error fetching current weather data")
+        cityDisplay.innerText = "City not found"
+        return
+    }
+    const dataNow = await responseNow.json()
+
+    document.querySelector("#nowTemp").innerHTML = `${Math.round(dataNow.main.temp)}°`
+    displayIcons(dataNow.weather[0].icon, weatherNow)
+
+    const responseForecast = await fetch(apiURLForecast)
+    if (!responseForecast.ok) {
+        console.log("Error fetching forecast data")
+        cityDisplay.innerText = "City not found"
+        return
+    }
+    dataForecast = await responseForecast.json()
+    console.log("Check weather function called for city:" + city)
+    createSummary()
+}
+checkWeather()
+function createSummary(){
+    forecastArray = []
+    dailySummary = []
+    dataForecast.list.forEach(item => {
+        const date = item.dt_txt.split(" ")[0]
+        const time = item.dt_txt.split(" ")[1]
+        var hourObject = {time: time, temperature: item.main.temp, icon: item.weather[0].icon, pop: item.pop * 100}
+            if (!forecastArray[date]) {
+            forecastArray[date] = []
+        }
+        forecastArray[date].push(hourObject)
+    })
+        var temps = []
+        dailySummary = Object.keys(forecastArray).map(date => {
+            var temps = []
+            forecastArray[date].forEach(item => {
+                temps.push(item.temperature)
+            })
+            var pops = []
+            forecastArray[date].forEach(item => {
+                pops.push(item.pop)
+            })
+            var dayHour = forecastArray[date].find(item => item.time === "12:00:00")
+            dayHour ? dayIcon = dayHour.icon : dayIcon = undefined
+            return {
+                date,
+                highestTemp: Math.max(...temps),
+                lowestTemp: Math.min(...temps),
+                dayIcon,
+                highestPop: Math.max(...pops)
+            }
+        })
+    
+    console.log(forecastArray)
+    console.log(dailySummary)
+    displayTemps(dailySummary, dayOne, 1)
+    displayTemps(dailySummary, dayTwo, 2)
+    displayTemps(dailySummary, dayThree, 3)
+    displayTemps(dailySummary, dayFour, 4)
+    displayIcons(dailySummary[1].dayIcon, dayOne)
+    displayIcons(dailySummary[2].dayIcon, dayTwo)
+    displayIcons(dailySummary[3].dayIcon, dayThree)
+    displayIcons(dailySummary[4].dayIcon, dayFour)
+    displayWeekDay(dailySummary[1].date, dayOne)
+    displayWeekDay(dailySummary[2].date, dayTwo)
+    displayWeekDay(dailySummary[3].date, dayThree)
+    displayWeekDay(dailySummary[4].date, dayFour)
+    displayPop(dailySummary[1].highestPop, dayOne)
+    displayPop(dailySummary[2].highestPop, dayTwo)
+    displayPop(dailySummary[3].highestPop, dayThree)
+    displayPop(dailySummary[4].highestPop, dayFour)
+}
+
+// EVENT LISTENERS
+
+pinIcon.addEventListener("click", (event) => {
+    expandLocation()
+})
+weatherButton.forEach(button => {
+    button.addEventListener("focus", function(event) {
+        scaleParent(event)
+    })
+})
+weatherButton.forEach(button => {
+    button.addEventListener("blur", function(event) {
+        descaleParent(event)
+    })
+})
+setButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    setLocation()
+})
+searchInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        setLocation()
+    }
+})
+palletButton.addEventListener("click", (event) => {
+    hideShow(event.target, colorButtonContainer)
+})
+colorButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        changeColorScheme(event)
+        hideShow(event.target.parentElement, palletButton)
+    })
+})
+
+//FUNCTIONS
+function displayIcons(iconKey, id) {
+    id.querySelector("i").className = ""
+    switch (iconKey) {
+        case "01d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-sun";
+            break;
+        case "02d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-sun";
+            break;
+        case "03d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud";
+            break;
+        case "04d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud";
+            break;
+        case "09d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers";
+            break;
+        case "10d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers-heavy";
+            break;
+        case "11d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers-heavy";
+            break; 
+        case "13d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-snowflake";
+            break;  
+        case "50d": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers";
+            break;     
+        case "01n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-moon";
+            break;
+        case "02n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-moon";
+            break;
+        case "03n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud";
+            break;
+        case "04n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud";
+            break;
+        case "09n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers";
+            break;
+        case "10n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers-heavy";
+            break;
+        case "11n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers-heavy";
+            break; 
+        case "13n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-snowflake";
+            break;  
+        case "50n": 
+            id.querySelector("i").className = "weatherButton fa-solid fa-cloud-showers";
+            break;  
+        
+    }
+}
+function displayTemps(array, id, index) {
+    id.querySelector(".temp").innerText = (Math.round(array[index].highestTemp) + "°/" + Math.round(array[index].lowestTemp) + "°")
+}
+function displayWeekDay(date, id) {
+    var offDate = new Date(date)
+    var formattedDate = new Date(Date.UTC(offDate.getUTCFullYear(), offDate.getUTCMonth(),
+    offDate.getUTCDate(), offDate.getUTCHours(),
+    offDate.getUTCMinutes(), offDate.getUTCSeconds()))
+    var dayNumber = formattedDate.getUTCDay()
+    var dayString = ""
+    switch(dayNumber) {
+        case 0:
+            dayString = "Sun"
+            break
+        case 1:
+            dayString = "Mon"
+            break
+        case 2:
+            dayString = "Tue"
+            break
+        case 3:
+            dayString = "Wed"
+            break
+        case 4:
+            dayString = "Thu"
+            break
+        case 5:
+            dayString = "Fri"
+            break
+        case 6:
+            dayString = "Sat"
+            break
+    }
+    id.querySelector(".dayName").innerText = dayString
+
+}
+function displayPop(pop, id) {
+    id.querySelector(".pop").innerText = pop + "%"
+}
+function expandLocation() {
+    pinIconContainer.style.display = "none"
+    searchForm.style.display = "flex"
+    requestAnimationFrame(() => {
+        searchForm.style.width = "100%";
+    });
+    setTimeout(() => {
+        setButton.style.display = "block";
+    }, 750);
+    searchInput.focus()
+}
+function setLocation() {
+    city = searchInput.value
+    cityDisplay.innerText = city
+    checkWeather()
+    console.log(city)
+    searchForm.style.display = "none"
+    searchForm.style.width = 0
+    setButton.style.display = "none"
+    pinIconContainer.style.display = "flex"
+}
+function scaleParent(event) {
+    event.target.focus()
+    console.log("scale function called")
+    requestAnimationFrame(() => {
+        event.target.parentElement.style.transform = "scale(1.5)"
+        if (event.target.parentElement.querySelector(".temp")) {
+            event.target.parentElement.querySelector(".temp").style.visibility = "visible"
+        }
+        if (event.target.parentElement.querySelector(".popContainer")) {
+            event.target.parentElement.querySelector(".popContainer").style.visibility = "visible"
+        }
+    })
+}
+function descaleParent(event) {
+    console.log("descale function called")
+    requestAnimationFrame(() => {
+        event.target.parentElement.style.transform = "scale(1)"
+        if (event.target.parentElement.querySelector(".temp")) {
+            event.target.parentElement.querySelector(".temp").style.visibility = "hidden"
+        }
+        if (event.target.parentElement.querySelector(".popContainer")) {
+            event.target.parentElement.querySelector(".popContainer").style.visibility = "hidden"
+        }
+    })
+}
+function hideShow(swap1, swap2) {
+    swap1.classList.toggle("hidden")
+    swap2.classList.toggle("hidden")
+}
+function changeColorScheme(event) {
+    console.log("change color scheme function called")
+    console.log(event.target.id)
+    switch(event.target.id) {
+        case "daySchemeButton":
+            root.style.setProperty("--mainColor", "rgb(255, 255, 255)")
+            root.style.setProperty("--accentColor", "rgb(236, 172, 75)")
+            root.style.setProperty("--textColor", "rgb(0, 0, 0)")
+            console.log("changed to day colorScheme")
+            break;
+        case "nightSchemeButton":
+            root.style.setProperty("--mainColor", "rgb(0, 0, 0)")
+            root.style.setProperty("--accentColor", "rgb(198, 103, 88)")
+            root.style.setProperty("--textColor", "rgb(255, 255, 255)")
+            break;
+        case "monetSchemeButton":
+            root.style.setProperty("--mainColor", "rgb(171, 203, 204)")
+            root.style.setProperty("--accentColor", "rgb(255, 255, 255)")
+            root.style.setProperty("--textColor", "rgb(138, 96, 118)")
+            break;
+        case "vangoghSchemeButton":
+            root.style.setProperty("--mainColor", "rgb(40, 40, 90)")
+            root.style.setProperty("--accentColor", "rgb(229, 192, 98)")
+            root.style.setProperty("--textColor", "rgb(243, 237, 191)")
+            break;
+    }
+}
